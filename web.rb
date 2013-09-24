6823b95cebaeb8922314cf7bf6e9004101259e5b
Takamasa Suzuki <webcrafts098@gmail.com>
2013-09-24 19:58:11 +0900

    add fife

--- web.rb
+++ web.rb
@@ -2,6 +2,12 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 require 'haml'
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+    :adapter => 'sqlite3',
+    :database => 'db/bbs.sqlite'
+)
 
 set :haml, {:format => :html5 }
 
@@ -9,8 +15,12 @@ configure :development do
   Slim::Engine.set_default_options :pretty => true
 end
 
+class Sample < ActiveRecord::Base
+end
+
 get '/' do
     name = ENV['NAME']
     @message = "Hello, " << name
+    @bbs = Sample.all
     haml:HelloWorld
 end
